a2424495c4e41c6712e0f2ea7c6df9130d90fc42
Zubarev Dmitry <dmitryshikary@yandex.ru>
2025-04-08 20:36:30 +0300

    add mapping in ItemConverter
    Signed-off-by: Zubarev Dmitry <dmitryshikary@yandex.ru>

--- src/main/java/com/example/shareIt/item/service/converter/ItemConverter.java
+++ src/main/java/com/example/shareIt/item/service/converter/ItemConverter.java
@@ -4,11 +4,13 @@ import com.example.shareIt.item.dto.ItemUpdateDto;
 import com.example.shareIt.item.model.Item;
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring")
 public interface ItemConverter {
+    @Mapping(target = "owner", ignore = true)
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void fromDto(ItemUpdateDto itemUpdateDto, @MappingTarget Item item);
 }
