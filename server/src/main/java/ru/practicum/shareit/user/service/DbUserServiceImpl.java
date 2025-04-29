cccb6cacad1cc9540364d78eda6b32cafade8d47
Zubarev Dmitry <dmitryshikary@yandex.ru>
2025-04-29 18:28:00 +0300

    code review

--- server/src/main/java/ru/practicum/shareit/user/service/DbUserServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/user/service/DbUserServiceImpl.java
@@ -8,6 +8,7 @@ import com.example.shareIt.user.model.User;
 import com.example.shareIt.user.storage.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
 
@@ -16,19 +17,17 @@ import java.util.List;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Transactional(propagation = Propagation.REQUIRES_NEW)
 public class DbUserServiceImpl implements UserService {
     private final UserRepository repository;
 
     @Override
-    @Transactional
     public UserDto create(UserDto dto) {
         User user = repository.save(UserMapper.mapDtoToUser(dto));
         return UserMapper.mapUserToDto(user);
     }
 
     @Override
-    @Transactional
     public UserDto update(UserDto dto, Long id) {
         User user = repository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Обновляемый пользователь не найден"));
@@ -51,19 +50,20 @@ public class DbUserServiceImpl implements UserService {
     }
 
     @Override
-    @Transactional
     public void delete(Long id) {
         User user = repository.getReferenceById(id);
         repository.delete(user);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<UserDto> findAll() {
         List<User> users = repository.findAll();
         return UserMapper.mapUserToDto(users);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public UserDto findById(Long id) {
         User user = repository.findById(id).orElseThrow();
         return UserMapper.mapUserToDto(user);
