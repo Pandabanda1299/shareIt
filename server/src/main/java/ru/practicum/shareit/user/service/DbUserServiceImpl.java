fc2fedb1426cdca666c33ff7d42a5977424e5298
Zubarev Dmitry <dmitryshikary@yandex.ru>
2025-05-04 21:49:24 +0300

    добавление в pull request

--- server/src/main/java/ru/practicum/shareit/user/service/DbUserServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/user/service/DbUserServiceImpl.java
@@ -1,33 +1,34 @@
-package com.example.shareIt.user.service;
+package ru.practicum.shareit.user.service;
 
-import com.example.shareIt.exception.ConflictException;
-import com.example.shareIt.exception.NotFoundException;
-import com.example.shareIt.user.dto.UserDto;
-import com.example.shareIt.user.mapper.UserMapper;
-import com.example.shareIt.user.model.User;
-import com.example.shareIt.user.storage.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.shareit.exception.ConflictException;
+import ru.practicum.shareit.exception.NotFoundException;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.mapper.UserMapper;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.storage.UserRepository;
 
 import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-@Transactional(propagation = Propagation.REQUIRES_NEW)
+@Transactional(readOnly = true)
 public class DbUserServiceImpl implements UserService {
     private final UserRepository repository;
 
     @Override
+    @Transactional
     public UserDto create(UserDto dto) {
         User user = repository.save(UserMapper.mapDtoToUser(dto));
         return UserMapper.mapUserToDto(user);
     }
 
     @Override
+    @Transactional
     public UserDto update(UserDto dto, Long id) {
         User user = repository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Обновляемый пользователь не найден"));
@@ -50,22 +51,22 @@ public class DbUserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public void delete(Long id) {
         User user = repository.getReferenceById(id);
         repository.delete(user);
     }
 
     @Override
-    @Transactional(readOnly = true)
     public List<UserDto> findAll() {
         List<User> users = repository.findAll();
         return UserMapper.mapUserToDto(users);
     }
 
     @Override
-    @Transactional(readOnly = true)
     public UserDto findById(Long id) {
-        User user = repository.findById(id).orElseThrow();
+        User user = repository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Пользователь " + id + " не найден"));
         return UserMapper.mapUserToDto(user);
     }
 }
